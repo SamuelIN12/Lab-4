647a2fda26dbf9f09d6e22c19994c7a3c694b9ef
SamuelIN12 <32582282+SamuelIN12@users.noreply.github.com>
2017-10-30 21:39:06 -0600

    commit 1

---
+++ Order.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+
+/**
+ *
+ * @author samueliregbu
+ */
+public class Order {
+    
+    private Date dateReceived;
+    private Double price;
+    private int orderID;
+    private Customer customer;
+    
+    public ArrayList<OrderLine> orderLine = new ArrayList<OrderLine>();
+
+   }
---
+++ OrderLine.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author samueliregbu
+ */
+public class OrderLine {
+    
+    private int Quantity1;
+    private Double Price1;
+    private Product Product1;
+    
+    }
