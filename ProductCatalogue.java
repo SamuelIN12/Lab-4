dcbc739d437911f1ccb2d3dd4ad51630158d87b0
SamuelIN12 <32582282+SamuelIN12@users.noreply.github.com>
2017-10-30 21:45:36 -0600

    commit 3

--- ProductCatalogue.java
+++ ProductCatalogue.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.ArrayList;
+import java.util.Iterator;
 /**
  *
  * @author samueliregbu
