9cdf6551aa9f143b97ca69bb92ee912dca133dd5
SamuelIN12 <32582282+SamuelIN12@users.noreply.github.com>
2017-10-30 21:47:39 -0600

    commit 5

--- Order.java
+++ Order.java
@@ -18,4 +18,41 @@ public class Order {
     
     public ArrayList<OrderLine> orderLine = new ArrayList<OrderLine>();
 
-   }
+    public Order(Customer Customer1)
+    {
+        price = 0.00;
+        orderID = 1;
+        dateReceived = new Date();
+        customer = Customer1;
+    }
+    
+    public Date getDateReceived()
+    {
+        return dateReceived;
+    }
+    
+    public Double calculatePrice()
+    {
+        Iterator<OrderLine> iterator = orderLine.iterator();
+        
+        while (iterator.hasNext()) {
+            OrderLine PresentOrder = iterator.next();
+            Product Product1 = PresentOrder.getProduct();
+            double Product_Price = Product1.getPrice();
+            double Product_Quantity = PresentOrder.getQuantity();
+            double Base_Price = Product_Price * Product_Quantity;
+            Base_Price = Base_Price - (Base_Price * customer.getDiscountRating());
+            
+            this.price += Base_Price;
+        }
+        
+        return price;
+    }
+    
+    public void addOrderItem(Product product, int quantity)
+    {
+        OrderLine newOrder = new OrderLine(product, 1, product.getPrice()); 
+        orderLine.add(newOrder);
+    }
+    
+}
