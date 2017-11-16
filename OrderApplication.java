88cc26f8936ca7a1069121108a9dab8d816f279d
SamuelIN12 <32582282+SamuelIN12@users.noreply.github.com>
2017-11-15 19:15:21 -0600

    commit 14

--- OrderApplication.java
+++ OrderApplication.java
@@ -16,6 +16,60 @@ public class OrderApplication {
     
     public ProductCatalogue Catalogue;
     
+    public static void main(String[] args){
+        
+        Customer customer = new Customer("Samuel Kurtis", "23 Rose Street", 2222.25f, 340.50f);
+        double price = 0.00;
+        
+        Order order = new Order(customer);
+        OrderApplication orderApp = new OrderApplication();
+        
+        if (orderApp.createOrder(order))
+        {
+            price = order.calculatePrice();
+            System.out.printf("The Order price is $%.2f\n", price);
+        }
+        
+        
+        order.printOrder();
+        
+        Scanner read = new Scanner(System.in);
+        char input = '0';
+        
+        do {
+            input = read.next().charAt(0);
+        } while ((input != '0' && input != '1'));
+        
+        if (input == '1') {
+            System.out.printf("Enter the orderline ID to be removed: ");
+            String index = read.next();
+            
+            if (order.removeOrderItem(Integer.parseInt(index) - 1) != null) {
+                System.out.printf("Removed");
+            }
+            else
+            {
+                System.out.print("unable to remove item. \n");
+            }
+        }
+        
+        else if (input != '0')
+        {
+            System.out.printf("Enter the orderline ID to be modified:" );
+            int index = Integer.parseInt(read.next());
+            
+            if (index <= order.OrderCount() && index != 0)
+            {
+                System.out.printf("Enter new quantity: ");
+                int quantity = Integer.parseInt(read.next());
+                
+                order.setQuantity(index - 1,  quantity);
+            }
+        }
+        
+    }
+    
+    
     public OrderApplication()
     {
     }
