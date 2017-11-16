74dd3afd05bb9650e0f0805af12dbebaa412a444
SamuelIN12 <32582282+SamuelIN12@users.noreply.github.com>
2017-11-15 18:18:59 -0600

    commit 10

--- OrderLine.java
+++ OrderLine.java
@@ -14,11 +14,18 @@ public class OrderLine {
     private Double Price1;
     private Product Product1;
     
+    public OrderLine()
+    {
+        Product product = new Product();
+        Quantity1 = 0;
+        Price1 = 0.00;
+    }
+    
     public OrderLine(Product Product2, int Quantity2, Double Price2)
     {
-        this.Quantity1 = Quantity2;
-        this.Price1 = Price2;
-        this.Product1 = Product2;
+        Quantity1 = Quantity2;
+        Price1 = Price2;
+        Product1 = Product2;
     }
     
     public Product getProduct()
@@ -34,5 +41,14 @@ public class OrderLine {
     public Double getPrice()
     {
         return Price1;
+    }
+    public void setPrice(Double price_2)
+    {
+        Price1 = price_2;
+    }
+    
+    public void setQuantity(int quantity_2)
+    {
+        Quantity1 = quantity_2;
     }
 }
